19a6c3a932e865fcef6a9f8ca85a1a9433bf43fb
Mila Kostic <milakostic.eng@gmail.com>
2022-08-01 22:23:18 +0200

    welcome

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Header from "./components/header/Header";
+import Body from "./components/body/Body";
 
 function App() {
   return (
     <div>
       {/* hello */}
       <Header />
+      <Body />
     </div>
   );
 }
---
+++ src/components/body/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Welcome from "./welcome/Welcome";
+
+function Body() {
+  return (
+    <div className="body-container">
+      <Welcome />
+    </div>
+  );
+}
+
+export default Body;
