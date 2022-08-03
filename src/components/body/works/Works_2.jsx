2078bb0edf78d797f480ec4960e2974618b4a51d
Mila Kostic <milakostic.eng@gmail.com>
2022-08-03 21:18:05 +0200

    for styles

--- src/components/body/works/Works_2.jsx
+++ src/components/body/works/Works_2.jsx
@@ -3,9 +3,20 @@ import React from "react";
 function Works_2() {
   return (
     <div className="works2-container">
-      <h2>Works2</h2>
+      <div>
+        <h2>Works2</h2>
+      </div>
+      <div>
+        <p className="p">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+          exercitationem hic numquam molestias temporibus deleniti esse, quia
+          autem maiores laudantium? Ab facere, natus voluptatibus veritatis
+          deserunt obcaecati voluptatum tempora ut atque repellendus ratione
+          saepe. Quisquam inventore officiis esse mollitia nihil?
+        </p>
+      </div>
     </div>
   );
 }
-
+/*ispraviti*/
 export default Works_2;
